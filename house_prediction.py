954c12abcc66ff40fdf91cd3221a729cb7790cae
karthik <karthikmnmudgodu@gmail.com>
2024-05-30 23:04:07 +0530

    housing_price_prediction

--- house_prediction.py
+++ house_prediction.py
@@ -279,3 +279,16 @@ with open("forest_reg.pkl","wb") as file:
 
 
 # while opening the file just use in rb and give name of the file
+#grid searh for finding the optimum parameter
+
+
+from sklearn.model_selection import GridSearchCV
+param_grid = [
+{'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+{'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+]
+forest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+scoring='neg_mean_squared_error',
+return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
